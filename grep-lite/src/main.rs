84ac7331a2d3933903c02b26f7bfd877745f0f1a
Darshan Mohan Tak <darshan.mohan@juspay.in>
2024-04-23 01:18:08 +0530

    grep-lite with vectors

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -1,3 +1,4 @@
+#![allow(unused)]
 fn grep_lite() {
     let search_term = "picture";
     let mut line_number = 1;
@@ -42,10 +43,58 @@ fn playing_with_arrays() {
         }
         println!("\tSum of the array is {}",sum)
     }
-    
+}
+
+fn grep_lite_with_vectors(){
+    let context_lines = 1;
+    let needle = "same";
+    let haystack = "\
+    Every face, every shop,
+    bedroom window, public-house, and 
+    dark square is a picture
+    feversihly turned--in search of what?
+    It is the same with books.
+    What to we seek
+    through millions of pages.";
+
+    let mut tags : Vec<usize> = Vec::new();
+    let mut ctx : Vec<Vec<(usize,String)>> = Vec::new();
+
+    for (i,line) in haystack.lines().enumerate(){
+        if line.contains(needle){
+            tags.push(i);
+
+            let v = Vec::with_capacity(2*context_lines+1);
+            ctx.push(v);           
+        }
+    }
+
+    if tags.len()==0{ //can use `tags.is_empty()``
+        return;
+    }
+
+    for (i,line) in haystack.lines().enumerate(){
+        for (j,tag) in tags.iter().enumerate(){
+            let lower_bound = tag.saturating_sub(context_lines);
+            let upper_bound = tag + context_lines;
+            
+            if (i>=lower_bound) && (i<=upper_bound){
+                let line_as_string = String::from(line);
+                let local_ctx = (i,line_as_string);
+                ctx[j].push(local_ctx);
+            }
+        }
+    }
+    for local_ctx in ctx.iter(){
+        for &(j,ref line) in local_ctx.iter(){
+            let line_num = j+1;
+            println!("{} {}",line_num,line);
+        }
+    }
 }
 fn main() {
     println!("Grep-Lite");
-    grep_lite();
-    playing_with_arrays();
+    // grep_lite();
+    // playing_with_arrays();
+    grep_lite_with_vectors();
 }
