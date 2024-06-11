b24fe4d9d0c6af9338841cd0f0e63515790faaf6
darshantak <darshanmohant@gmail.com>
2024-06-12 01:35:13 +0530

    added async file io ops

--- async-rust/src/main.rs
+++ async-rust/src/main.rs
@@ -1,7 +1,7 @@
 #[allow(unused)]
 mod modules;
-use modules::async_exp;
 use futures::executor::block_on;
+use modules::async_exp;
 use modules::futures_exp;
 
 #[tokio::main]
@@ -9,12 +9,16 @@ async fn main() {
     println!("Hello, world!");
     async_exp::intro();
 
-    let async_http = futures_exp::http_requests::fetch_concurrently().await;
-    match async_http{
-        Ok(_) => println!("This ran successfully."),
-        Err(err) => println!("Exited this with error : {}", err)
-    }
+    // let async_http = futures_exp::http_requests::fetch_concurrently().await;
+    // match async_http{
+    //     Ok(_) => println!("This ran successfully."),
+    //     Err(err) => println!("Exited this with error : {}", err)
+    // }
 
+    if let Ok(()) = futures_exp::file_io::file_example().await {
+        println!("This is successfull");
+    }
+    
     // if let Ok(_) = futures_exp::http_requests::fetch_concurrently().await{
     //     println!("Worked fine");
     // }
